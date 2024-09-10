012153f9f8a01ba25835d70179c6f5e512cddc84
Kemal Škrijelj <kemalskrijelj6@gmail.com>
2024-09-10 21:29:04 +0200

    first next.js commit

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link"
+export default function About () {
+  return (
+  <div>
+    <h1>About</h1>
+    <p>This is about page</p>
+    <Link href='/'>Go to HomePage</Link>
+  </div>
+  )
+}
