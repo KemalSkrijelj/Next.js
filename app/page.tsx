15a6c9ef49451761a1e59e85e434e3ecb7a21d3a
Kemal Škrijelj <kemalskrijelj6@gmail.com>
2024-09-19 23:59:11 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,7 @@
-import createPost from "@/server/actions/create-posts";
-import getPosts from "@/server/actions/get-posts";
-import Image from "next/image";
-import PostBtn from "@/components/post-btn";
 export default async function Home() {
-  const { error, succes } = await getPosts();
-  if (error) {
-    throw new Error(error);
-  }
-  if (succes)
     return (
       <main>
-        {succes.map((post) => (
-          <div key={post.id}>
-            <h2>{post.title}</h2>
-          </div>
-        ))}
-        <form action={createPost}>
-          <input
-            className="bg-black"
-            type="text"
-            name="title"
-            placeholder="Title"
-          />
-          <PostBtn />
-        </form>
-        <Image src="./vercel.svg" alt="Vercel logo" width={72} height={16} />
+        <h1>Hey</h1>
       </main>
     );
 }
