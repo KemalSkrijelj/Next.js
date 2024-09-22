b16019a093c401e960aa58984131b46b53a4caf6
Kemal Škrijelj <kemalskrijelj6@gmail.com>
2024-09-22 19:49:21 +0200

    Creating NavBar

---
+++ components/navigation/logo.tsx
@@ -0,0 +1,30 @@
+export default function Logo() {
+  return (
+    <svg
+      width="212"
+      height="89"
+      viewBox="0 0 212 89"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="212" height="89" fill="url(#pattern0)" />
+      <defs>
+        <pattern
+          id="pattern0"
+          patternContentUnits="objectBoundingBox"
+          width="1"
+          height="1"
+        >
+          <use
+            transform="matrix(0.00118064 0 0 0.00281231 0 -0.000590319)"
+          />
+        </pattern>
+        <image
+          id="image0_14_2"
+          width="847"
+          height="356"
+        />
+      </defs>
+    </svg>
+  );
+}
